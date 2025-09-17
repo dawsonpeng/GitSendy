08bcea824fb3faab6bc3da3e0216a20a8b5e77f2
Dawson <dpeng1@hwemail.com>
2025-09-17 12:09:25 -0700

    finished initrepo, rmrepo, hash, and addindex

--- Git.java
+++ Git.java
@@ -1,10 +1,54 @@
 import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Git {
-    public static void initRepo() {
-        File dir = new File("myDirectory");
-        if(!dir.exists()) {
-            dir.mkdir();
-        } else System.out.println("File already exists dawg🥀");
+    public static void initRepo() throws IOException {
+        File git = new File("git");
+        File objects = new File("git", "objects");
+        File index = new File("git", "index");
+
+        if(!git.exists()) {
+            git.mkdir();
+        } else System.out.println("git already exists dawg🥀");
+
+        if(!objects.exists()) {
+            objects.mkdir();
+        }
+
+        if(!index.exists()) {
+            index.createNewFile();
+        }
+    }
+
+    public static void rmRepo() {
+        File objects = new File("git", "objects");
+        File index = new File("git", "index");
+        File git = new File("git");
+
+        if(objects.exists()) {
+            objects.delete();
+        }
+
+        if(index.exists()) {
+            index.delete();
+        }
+
+        if(git.exists()) {
+            git.delete();
+        }
+    }
+
+    public static String hash(String text) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] messageDigest = md.digest(text.getBytes());
+        BigInteger no = new BigInteger(1, messageDigest);
+        String hashtext = no.toString(16);
+        while (hashtext.length() < 40) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
     }
 }
---
+++ Tester.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class Tester {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        
+    }
+}
