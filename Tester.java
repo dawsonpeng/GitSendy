3812793d1564e53b953f4e32b66da5fefb5582a5
Dawson <dpeng1@hwemail.com>
2025-09-18 10:23:31 -0700

    prepared tester

--- Tester.java
+++ Tester.java
@@ -3,6 +3,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Tester {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Git.initRepo();
         Git.createBlob("file.txt");
     }
 }
